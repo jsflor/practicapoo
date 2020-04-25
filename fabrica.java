9330b96811816bed4b8aebb960559756ea675ce8
sebastianflor <sebastian-flor@hotmail.com>
2020-04-25 13:11:45 +0200

    initial state

---
+++ fabrica.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class fabrica here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class fabrica{
+    public static void main(String[] args){
+        System.out.println("Welcome!");
+    }
+}
